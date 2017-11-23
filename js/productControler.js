944e11ede750123af6c7d1a32fadb74eb1f3623f
Tom Ormiston <tom@appijumbo.com>
2017-11-23 13:49:32 +0000

    try different approaches to dom traversing

--- js/productControler.js
+++ js/productControler.js
@@ -23,6 +23,49 @@ allOffers = [];
 
   function aquireOffers(){
 
+/** ways to travertse the DOM and find the data  ****/
+  //var offers = document.querySelectorAll(".offer");
+
+  var offers = document.querySelectorAll(".offer");
+
+  for(i =0; i<offers.length; i++){
+    var parentOffer = offers[i].parentElement; // gives article node
+    //  Put parentOffer into a 'jQuery wrapper' so can use jquery methods
+    var title2 = $(parentOffer).children().children("h3")[0].innerText;
+    var sku3 = parentOffer.attributes[0].nodeValue;
+    var url3 = parentOffer.childNodes[1].childNodes[1].attributes.nodeValue;
+    var priceStr2 = $(parentOffer).children("span.price")[0].innerText;
+    var priceStr3 = parentOffer.childNodes[3].childNodes[0].nodeValue.trim();
+  }
+
+/***
+Some more thoughts,
+getElementsByClassName returns *live* DOM elements.
+Changes made to those DOM elements will be updated in its list.
+
+I'dont belive querySelecAll by class will do this?
+
+Proably a good reason to use jqery selector and
+getElementsByClassName with jquery 'like' js selectors
+/*** the initial approach ***/
+
+
+var offers_c = document.getElementsByClassName("offer");
+
+for(i =0; i<offers_c.length; i++){
+  var parentOffer_c = offers_c[i].parentElement; // gives article node
+  //  Put parentOffer into a 'jQuery wrapper' so can use jquery methods
+  var title2_c = $(parentOffer_c).children().children("h3")[0].innerText;
+  var sku3_c = parentOffer_c.attributes[0].nodeValue;
+  var url3_c = parentOffer_c.childNodes[1].childNodes[1].attributes.nodeValue;
+  var priceStr2_c = $(parentOffer_c).children("span.price")[0].innerText;
+  var priceStr3_c = parentOffer_c.childNodes[3].childNodes[0].nodeValue.trim();
+}
+
+
+
+
+
     var article = $('.offer');
 
   	$('.offer').each(function(index){ // Sanitise data and save to an object
@@ -30,7 +73,6 @@ allOffers = [];
   		var title = $(parent).find("h3")[0].childNodes[0].data.trim();
   		var sku = $(parent).attr( "data-sku");
       var url = $(parent).find('img').attr('src');
-
 
       //var priceData = $(parent).find('span.price')[0].childNodes[0].data;
       var priceStr = $(parent).find('span.price').html();
@@ -41,7 +83,7 @@ allOffers = [];
         priceClean = priceClean/100;
       };
 
-      var offerStr = $(parent).find('span.offer').html().trim();
+     var offerStr = $(parent).find('span.offer').html().trim();
 
   		var productObj = {
   			SKU: sku,
